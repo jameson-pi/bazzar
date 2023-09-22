ac699d6305987ea04a37f8dacea7fae1ae5d29e6
Jameson Piscitello <75795824+jameson-pi@users.noreply.github.com>
2023-09-22 11:59:15 -0500

    customize it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+function GoToLogIn() {
+  return (
+    <button id="login">
+      Log in
+    </button>
+  )
+}
+function Topbar(){
+  <header>
+    <h1>
+      Jamesons website
+    </h1>
+    <GoToLogIn />
+  </header>
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <Topbar />
+    <h1>
+      Welcome to my website!!!
+    </h1>
     </div>
+
   );
 }
 
