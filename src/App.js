3f769ad42d77ad93d648de10a443939a29ccf17c
Jameson Piscitello <75795824+jameson-pi@users.noreply.github.com>
2023-09-22 15:31:45 -0500

    try to add download

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+function DownloadUrl(url){
+  window.open(url, '_self');
+}
 function GoToLogIn() {
   return (
-    <button id="login">
+    <button id="login" onClick="window.open('virus.txt', '_self');">
       Log in
     </button>
   )
 }
-function DownloadUrl(url){
-  window.open(url, '_self');
-}
+
 function Topbar(){
   <div>
     <h1>
@@ -21,6 +22,12 @@ function Topbar(){
 function App() {
   return (
     <div>
+        <header>
+    <h1>
+      Jamesons website
+    </h1>
+    <GoToLogIn />
+  </header>
       <Topbar></Topbar>
     <h1>
       Welcome to my website!!!
