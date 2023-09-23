12dd25823dd148d6af8cf4883141edb1a8f3b935
Jameson Piscitello <piscitello284@gmail.com>
2023-09-23 09:32:00 -0500

    add stuff to pages

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import virus from './virus.txt';
-function DownloadUrl(url){
-  window.open(url, '_self');
-}
-function GoToLogIn() {
-  return (
-    <a
-    href={virus}
-    download="not_virus"
-    target="_blank"
-    rel="noreferrer"
-  >
-    <button>Log in</button>
-  </a>
-  )
-}
-
-function Topbar(){
-  <div>
-    <h1>
-      Jamesons website
-    </h1>
-    <GoToLogIn />
-  </div>
-}
 function App() {
   return (
     <div>
-        <header>
-    <h1>
-      Jamesons website
-    </h1>
-    <GoToLogIn />
-  </header>
-      <Topbar></Topbar>
-    <h1>
-      Welcome to my website!!!
-    </h1>
+      hi
     </div>
 
   );
